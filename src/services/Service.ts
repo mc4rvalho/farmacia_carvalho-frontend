6f63b8591e28081d4206056fa432889c928c3b32
mc4rvalho <mcarvalho093@gmail.com>
2026-02-25 23:29:28 -0300

    chore: create Service

---
+++ src/services/Service.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-wrapper-object-types */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
+import axios from "axios"
+
+const api = axios.create({
+  baseURL: "https://farmacia-js12.onrender.com/",
+})
+
+export const buscar = async (
+  url: string,
+  setDados: Function,
+  header?: Object,
+) => {
+  const resposta = await api.get(url, header)
+  setDados(resposta.data)
+}
+
+export const cadastrar = async (
+  url: string,
+  dados: Object,
+  setDados: Function,
+  header?: Object,
+) => {
+  const resposta = await api.post(url, dados, header)
+  setDados(resposta.data)
+}
+
+export const atualizar = async (
+  url: string,
+  dados: Object,
+  setDados: Function,
+  header?: Object,
+) => {
+  const resposta = await api.put(url, dados, header)
+  setDados(resposta.data)
+}
+
+export const deletar = async (url: string, header: Object) => {
+  await api.delete(url, header)
+}
